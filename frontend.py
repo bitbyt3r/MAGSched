9d36865a68e4cfd7691e42db273375c9f44389bd
Mark Murnane <mark@hackafe.net>
2023-01-01 01:03:00 -0500

    Fixing tracks selection

--- frontend.py
+++ frontend.py
@@ -81,7 +81,7 @@ def search(collection):
                     print(f"Filtering on {key} in {request.args[key]}")
                     filtered = list(
                         filter(lambda x: request.args.get(key) in x[key], filtered))
-                if isinstance(prototype[key], bool):
+                elif isinstance(prototype[key], bool):
                     print(f"Filtering on {key} == True")
                     filtered = list(
                         filter(lambda x: x[key] == (request.args.get(key).lower() == "true"), filtered)
