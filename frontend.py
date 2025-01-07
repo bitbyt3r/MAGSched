a0515b47647ce08ff129ccd41f90bf3fe355e1f3
Mark Murnane <mark@hackafe.net>
2025-01-06 22:39:35 -0500

    Adding searchable frab

--- frontend.py
+++ frontend.py
@@ -198,122 +198,125 @@ def make_guid(collection, id):
     gid = uuid.uuid3(uuid.NAMESPACE_URL, url)
     return str(gid)
 
+def sessions_to_frab(sessions):
+    locations = get_collection("locations")
+    location_lookup = {x.id: x for x in locations}
+    soup = BeautifulSoup(features='xml')
+    schedule = soup.new_tag("schedule")
+    soup.append(schedule)
+    generator = soup.new_tag("generator", attrs={"name": "magsched", "version": "1.0"})
+    schedule.append(generator)
+    version = soup.new_tag("version")
+    schedule.append(version)
+    version.string = "Guidebook"
+    conference = soup.new_tag("conference")
+    schedule.append(conference)
+    for tagname in [
+        "acronym",
+        "title",
+        "start",
+        "end",
+        "days",
+        "timeslot_duration",
+        "time_zone_name",
+        "base_url"
+    ]:
+        tag = soup.new_tag(tagname)
+        conference.append(tag)
+        tag.string = getattr(config, tagname)
+    days = {}
+    for session in sessions:
+        day = session.start_time.astimezone(pytz.timezone(config.time_zone_name)).strftime("%Y-%m-%d")
+        if not day in days:
+            days[day] = {
+                "date": day,
+                "start": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).replace(hour=0, minute=0, second=0, microsecond=0).isoformat(),
+                "end": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).replace(hour=23, minute=45, second=0, microsecond=0).isoformat(),
+                "rooms": {}
+            }
+        for location in session.locations:
+            if not location in days[day]["rooms"]:
+                if not location in location_lookup:
+                    print(f"session {session.id} has invalid location {location}")
+                    continue
+                days[day]["rooms"][location] = {
+                    "id": location,
+                    "name": location_lookup[location].name,
+                    "events": [] 
+                }
+            duration = (session.end_time - session.start_time).seconds
+            days[day]["rooms"][location]["events"].append({
+                "id": session.id,
+                "date": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).isoformat(),
+                "start": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).strftime("%H:%M"),
+                "duration": f"{duration // 3600}:{int((duration % 3600)/60):02}",
+                "room": location_lookup[location].name,
+                "slug": f"{config.acronym}-{int(session.id) % 1000000}-sess",
+                "url": f"{config.base_url}/sessions/{session.id}",
+                "title": session.name,
+                "subtitle": "",
+                "track": session.tracks[0] if session.tracks else "",
+                "type": "",
+                "language": "en",
+                "abstract": BeautifulSoup(session.description).get_text(),
+                "description": BeautifulSoup(session.description).get_text(),
+                "logo": "https://www.magfest.org/assets/logo_magfest_lg.png",
+                "persons": "",
+                "links": "",
+                "attachments": ""
+            })
+    day_list = list(days.values())
+    day_list.sort(key=lambda x: x['start'])
+    for idx, day in enumerate(day_list):
+        day_tag = soup.new_tag("day", date=day['date'], end=day['end'], index=str(idx+1), start=day['start'])
+        schedule.append(day_tag)
+        for room in day['rooms'].values():
+            room_tag = soup.new_tag("room", guid=make_guid("locations", room['id']))
+            room_tag.attrs["name"] = room['name']
+            day_tag.append(room_tag)
+            for event in room['events']:
+                event_tag = soup.new_tag("event", guid=make_guid("sessions", event['id']), id=str(event['id']))
+                room_tag.append(event_tag)
+                for tagname in [
+                    "date",
+                    "start",
+                    "duration",
+                    "room",
+                    "slug",
+                    "url",
+                    "title",
+                    "subtitle",
+                    "track",
+                    "type",
+                    "language",
+                    "abstract",
+                    "description",
+                    "logo",
+                    "persons",
+                    "links",
+                    "attachments"
+                ]:
+                    tag = soup.new_tag(tagname)
+                    event_tag.append(tag)
+                    tag.string = event.get(tagname, "")
+                recording = soup.new_tag("recording")
+                event_tag.append(recording)
+                license = soup.new_tag("license")
+                recording.append(license)
+                optout = soup.new_tag("optout")
+                recording.append(optout)
+                optout.string = "false"
+    return soup
+
 @app.route("/frab", methods=["GET", "OPTIONS"])
 def frab():
     if request.method == "OPTIONS":
         return _build_cors_preflight_response()
     age = cache.get("frab-age")
     if (not age) or (time.time() - age > 60):
-        soup = BeautifulSoup(features='xml')
-        schedule = soup.new_tag("schedule")
-        soup.append(schedule)
-        generator = soup.new_tag("generator", attrs={"name": "magsched", "version": "1.0"})
-        schedule.append(generator)
-        version = soup.new_tag("version")
-        schedule.append(version)
-        version.string = "Guidebook"
-        conference = soup.new_tag("conference")
-        schedule.append(conference)
-        for tagname in [
-            "acronym",
-            "title",
-            "start",
-            "end",
-            "days",
-            "timeslot_duration",
-            "time_zone_name",
-            "base_url"
-        ]:
-            tag = soup.new_tag(tagname)
-            conference.append(tag)
-            tag.string = getattr(config, tagname)
-        
-        locations = get_collection("locations")
-        location_lookup = {x.id: x for x in locations}
-        days = {}
         sessions = get_collection("sessions")
         sessions.sort(key=lambda x: x.start_time)
-        for session in sessions:
-            day = session.start_time.astimezone(pytz.timezone(config.time_zone_name)).strftime("%Y-%m-%d")
-            if not day in days:
-                days[day] = {
-                    "date": day,
-                    "start": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).replace(hour=0, minute=0, second=0, microsecond=0).isoformat(),
-                    "end": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).replace(hour=23, minute=45, second=0, microsecond=0).isoformat(),
-                    "rooms": {}
-                }
-            for location in session.locations:
-                if not location in days[day]["rooms"]:
-                    if not location in location_lookup:
-                        print(f"session {session.id} has invalid location {location}")
-                        continue
-                    days[day]["rooms"][location] = {
-                        "id": location,
-                        "name": location_lookup[location].name,
-                        "events": [] 
-                    }
-                duration = (session.end_time - session.start_time).seconds
-                days[day]["rooms"][location]["events"].append({
-                    "id": session.id,
-                    "date": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).isoformat(),
-                    "start": session.start_time.astimezone(pytz.timezone(config.time_zone_name)).strftime("%H:%M"),
-                    "duration": f"{duration // 3600}:{int((duration % 3600)/60):02}",
-                    "room": location_lookup[location].name,
-                    "slug": f"{config.acronym}-{int(session.id) % 1000000}-sess",
-                    "url": f"{config.base_url}/sessions/{session.id}",
-                    "title": session.name,
-                    "subtitle": "",
-                    "track": session.tracks[0] if session.tracks else "",
-                    "type": "",
-                    "language": "en",
-                    "abstract": BeautifulSoup(session.description).get_text(),
-                    "description": BeautifulSoup(session.description).get_text(),
-                    "logo": "https://www.magfest.org/assets/logo_magfest_lg.png",
-                    "persons": "",
-                    "links": "",
-                    "attachments": ""
-                })
-        day_list = list(days.values())
-        day_list.sort(key=lambda x: x['start'])
-        for idx, day in enumerate(day_list):
-            day_tag = soup.new_tag("day", date=day['date'], end=day['end'], index=str(idx+1), start=day['start'])
-            schedule.append(day_tag)
-            for room in day['rooms'].values():
-                room_tag = soup.new_tag("room", guid=make_guid("locations", room['id']))
-                room_tag.attrs["name"] = room['name']
-                day_tag.append(room_tag)
-                for event in room['events']:
-                    event_tag = soup.new_tag("event", guid=make_guid("sessions", event['id']), id=str(event['id']))
-                    room_tag.append(event_tag)
-                    for tagname in [
-                        "date",
-                        "start",
-                        "duration",
-                        "room",
-                        "slug",
-                        "url",
-                        "title",
-                        "subtitle",
-                        "track",
-                        "type",
-                        "language",
-                        "abstract",
-                        "description",
-                        "logo",
-                        "persons",
-                        "links",
-                        "attachments"
-                    ]:
-                        tag = soup.new_tag(tagname)
-                        event_tag.append(tag)
-                        tag.string = event.get(tagname, "")
-                    recording = soup.new_tag("recording")
-                    event_tag.append(recording)
-                    license = soup.new_tag("license")
-                    recording.append(license)
-                    optout = soup.new_tag("optout")
-                    recording.append(optout)
-                    optout.string = "false"
+        soup = sessions_to_frab(sessions)
         full_frab = str(soup)
         cache["frab"] = full_frab
         cache["frab-age"] = time.time()
@@ -323,3 +326,10 @@ def frab():
     response = _cors(make_response(full_frab))
     response.mimetype = "text/xml"
     return response
+
+@app.route("/frab/filtered", methods=["GET", "OPTIONS"])
+def frab_filtered():
+    if request.method == "OPTIONS":
+        return _build_cors_preflight_response()
+    results = search("sessions")
+    return _cors(make_response(str(sessions_to_frab(results))))
