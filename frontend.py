e155d790cbdf197cc9e3572f6b88060c4d50fcf7
Mark Murnane <mark@hackafe.net>
2025-01-06 23:03:04 -0500

    Deserializing sessions

--- frontend.py
+++ frontend.py
@@ -5,6 +5,7 @@ import zoneinfo
 import time
 import uuid
 import pytz
+import json
 
 import database
 import config
@@ -331,5 +332,5 @@ def frab():
 def frab_filtered():
     if request.method == "OPTIONS":
         return _build_cors_preflight_response()
-    results = [database.Session.deserialize(x) for x in search("sessions")]
+    results = [database.Session.deserialize(json.dumps(x)) for x in search("sessions")]
     return _cors(make_response(str(sessions_to_frab(results))))
