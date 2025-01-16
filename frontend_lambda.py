d49342ec2bda6e9df3a2a0c1a50a388733fdc78b
Mark Murnane <mark@hackafe.net>
2025-01-15 22:44:22 -0500

    Using extract

--- frontend_lambda.py
+++ frontend_lambda.py
@@ -1,3 +1,4 @@
+import lxml
 from bs4 import BeautifulSoup
 import boto3
 import time
@@ -358,7 +359,6 @@ def frab():
     age = cache.get("frab-age")
     if (not age) or (time.time() - age > 60):
         sessions = get_collection("sessions")
-        print(sessions)
         sessions.sort(key=lambda x: x.start_time)
         soup = sessions_to_frab(sessions)
         full_frab = str(soup)
