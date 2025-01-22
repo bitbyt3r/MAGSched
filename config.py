872b2d21799aa714f8a8a5b11350a477f0fa75cd
Mark Murnane <mark@hackafe.net>
2025-01-22 18:14:13 -0500

    Turning off time loop

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ redis_host = os.environ.get("REDIS_HOST", "localhost")
 redis_port = int(os.environ.get("REDIS_PORT", "6379"))
 redis_db = int(os.environ.get("REDIS_DB", "0"))
 refresh_delay = float(os.environ.get("REFRESH_DELAY", "60"))
-time_loop = bool(os.environ.get("TIME_LOOP", "true").lower() == "true")
+time_loop = bool(os.environ.get("TIME_LOOP", "false").lower() == "true")
 time_zone = os.environ.get("TIME_ZONE", "America/New_York")
 
 acronym = os.environ.get("ACRONYM", "super2025")
